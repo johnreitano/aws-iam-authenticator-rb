75fc5c51c6eb8849e7660531f7d4e63d9a9a2cbb
Cameron Dutro <camertron@gmail.com>
2020-06-13 10:10:41 -0700

    Better memoized variable name

--- lib/aws-iam-authenticator-rb.rb
+++ lib/aws-iam-authenticator-rb.rb
@@ -1,6 +1,6 @@
 module AwsIamAuthenticatorRb
   def self.executable
-    @path ||= begin
+    @executable ||= begin
       pattern = File.expand_path(File.join('..', 'vendor', 'aws-iam-authenticator*'), __dir__)
       Dir.glob(pattern).first
     end
