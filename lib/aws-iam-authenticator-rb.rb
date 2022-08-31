b1ecab86325db7db9d750dd9edc1de1f0d03d826
John Reitano <jreitano@gmail.com>
2022-08-31 07:37:16 -0700

    use locally installed aws-iam-authenticator to work around executable missing from gem

--- lib/aws-iam-authenticator-rb.rb
+++ lib/aws-iam-authenticator-rb.rb
@@ -1,8 +1,5 @@
 module AwsIamAuthenticatorRb
   def self.executable
-    @executable ||= begin
-      pattern = File.expand_path(File.join('..', 'vendor', 'aws-iam-authenticator*'), __dir__)
-      Dir.glob(pattern).first
-    end
+    "aws-iam-authenticator"
   end
 end
